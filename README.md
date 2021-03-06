d5537fe546cc61111c22d6cb1a5cdb1fbffae06a
Raffi Rahwanda <raffirhwnda27@gmail.com>
2021-03-06 22:52:32 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Sample_Constraint_Layout" 
---
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.sampleconstraintlayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
